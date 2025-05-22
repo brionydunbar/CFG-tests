a830bbfb3797ed773c1344b0bf668d109630abf6
Briony Dunbar <brionydunbar@gmail.com>
2025-05-22 14:17:38 +0100

    Added Python session 3 files

---
+++ python_session3/if_statement_example.py
@@ -0,0 +1,12 @@
+"""
+The basic syntax is
+    if condition:
+        code to run
+"""
+
+# beach example
+temperature = 34
+is_sunny = False
+
+if (temperature >25) or is_sunny: # when the code reaches the if, this is where the decision is made
+    print("Go to beach") # this is only able to run if the if statement is true
